460d97563b7b821cc864811cd2f592df9678c22f
Thong <thongprolm2015@gmail.com>
2022-09-07 10:07:29 +0700

    fix add image bugs

--- Final Project/Program.cs
+++ Final Project/Program.cs
@@ -78,7 +78,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.ConfigureExceptionHandler();
+/*app.ConfigureExceptionHandler();*/
 
 app.UseMiddleware<ValidateTokenMiddleware>();
 
